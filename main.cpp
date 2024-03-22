86651599201c504505aee950fb2d30965af8d89a
passion1337 <162768394+passion1337@users.noreply.github.com>
2024-03-22 23:08:45 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,20 @@ namespace Rtl
 		uEncoded = __ROL8__(uEncoded, cookie & 0x3F);
 		return (pv)(uEncoded ^= cookie);
 	}
+
+	int walkVehList() {
+		PLDR_VECTOR_HANDLER_LIST vehHandlerList = (PLDR_VECTOR_HANDLER_LIST)RVA((u64)RtlAddVectoredExceptionHandler + 0x121, 7);
+		if (vehHandlerList->vehList->Flink == vehHandlerList->vehList) return 0;
+		int vehCnt = 0;
+		PLDR_VECTOR_HANDLER_ENTRY head = (PLDR_VECTOR_HANDLER_ENTRY)vehHandlerList->vehList;
+		PLDR_VECTOR_HANDLER_ENTRY curr = (PLDR_VECTOR_HANDLER_ENTRY)head;
+		do {
+			vehCnt++;
+			printf("[%p] Blink %p, Flink %p, EncodedPtr %p => Decode %p\n", curr, curr->listEntry.Blink, curr->listEntry.Flink, curr->EncodedPtr, Rtl::DecodePointer(curr->EncodedPtr));
+			curr = (PLDR_VECTOR_HANDLER_ENTRY)curr->listEntry.Flink;
+		} while (curr != head && (u64)curr != (u64)&vehHandlerList->vehList);
+		return vehCnt;
+	}
 }
 
 void TestHandler1() {
@@ -28,25 +42,14 @@ void TestHandler1() {
 
 void TestHandler2() {
 	return Sleep(0x43210);
+	
 }
 
 int main()
 {
-	printf("TestHandler1:0x%p\t\tTestHandler2:0x%p\n", TestHandler1, TestHandler2);
-
 	PVOID handler1 = AddVectoredExceptionHandler(0, (PVECTORED_EXCEPTION_HANDLER)TestHandler1);
 	PVOID handler2 = AddVectoredExceptionHandler(0, (PVECTORED_EXCEPTION_HANDLER)TestHandler2);
-
-	printf("VectoredHandler1:0x%p\tVectoredHandler2:0x%p\n\n\n", handler1, handler2);
-
-	PLDR_VECTOR_HANDLER_LIST vehHandlerList = (PLDR_VECTOR_HANDLER_LIST)RVA((u64)RtlAddVectoredExceptionHandler + 0x121, 7);
-	PLDR_VECTOR_HANDLER_ENTRY head = (PLDR_VECTOR_HANDLER_ENTRY)vehHandlerList->vehList;
-	PLDR_VECTOR_HANDLER_ENTRY curr = (PLDR_VECTOR_HANDLER_ENTRY)head;
-
-	printf("LdrpVectorHandlerList : %p=======\n", vehHandlerList);
-	do {
-		printf("[%p] Blink %p, Flink %p, EncodedPtr %p => Decode %p\n", curr, curr->listEntry.Blink, curr->listEntry.Flink, curr->EncodedPtr, Rtl::DecodePointer(curr->EncodedPtr));
-		curr = (PLDR_VECTOR_HANDLER_ENTRY)curr->listEntry.Flink;
-	} while (curr != head && (u64)curr != (u64)&vehHandlerList->vehList);
-	
+	printf("VectoredHandler1:0x%p\tVectoredHandler2:0x%p\n\n", handler1, handler2);
+	int cnt = Rtl::walkVehList();
+	printf("process has %d veh.\n", cnt);
 }
